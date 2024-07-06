181567b5ba3f5f81d09043e2b9e7144d58e62d69
Abhishek3423 <abhishekkamati304@gmail.com>
2024-07-06 14:17:27 +0545

    change

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,4 @@
-import Product from "../models/Product.js";
+import Product from "../models/product.js";
 import mongoose from "mongoose";
 import fs from 'fs';
 export const getTopProducts = async (req, res, next) => {
